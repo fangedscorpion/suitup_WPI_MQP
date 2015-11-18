48031911bb30fa983993deb1e068e95042616189
mandi1267 <mandi1267@gmail.com>
2015-11-18 11:15:35 -0500

    Add to netowrk monitor

--- software/GUI/WAG_bands/networkmonitor.h
+++ software/GUI/WAG_bands/networkmonitor.h
@@ -2,20 +2,25 @@
 #define NETWORKMONITOR_H
 
 #include <QQuaternion>
+#include <QString>
 
 class NetworkMonitor
 {
 public:
     NetworkMonitor(int portNumber);
+    ~NetworkMonitor();
 
 signals:
     void receievedQuaternion(QQuaternion quat);
+    void acceptedConnection(QString ipAddress);
 
 public slots:
     void sendMotorCommand();
 
 private:
-    int socket;
+    int clntSocket;
+    int recieveData(char *dataBuffer);
+    void sendData(char * serializedData, int dataLen);
 };
 
 #endif // NETWORKMONITOR_H
