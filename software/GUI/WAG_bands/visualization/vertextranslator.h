84a50cf8c0ce75796d0315d9d28f125657be4e65
mandi1267 <mandi1267@gmail.com>
2015-12-04 14:38:09 -0500

    Get adding vertices by index working; create function that allows to add plane defined by corner points and output plane

--- software/GUI/WAG_bands/visualization/vertextranslator.h
+++ software/GUI/WAG_bands/visualization/vertextranslator.h
@@ -12,19 +12,21 @@ public:
     VertexTranslator();
     const GLfloat *constData() const { return m_data.constData(); }
     int count() const { return m_count; }
-    int vertexCount() const { return m_count / 6; }
+    int vertexCount() const { return m_count /6; }
     int getPointCount() const { return pointCount; }
     const GLuint *getIndices() const { return vertexIndices.constData(); }
+    void addConvexPlane(QVector<QVector3D> planeCorners, QVector3D translatePlane, QQuaternion rotatePlane);
+
+
 
 private:
-    void quad(GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2, GLfloat x3, GLfloat y3, GLfloat x4, GLfloat y4);
-    void extrude(GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2);
     void add(const QVector3D &v, const QVector3D &n);
-    void add(const QVector3D &v);
-    void makeRectangularPrism(GLfloat width, GLfloat height, GLfloat depth);
-    void drawTriangle(QVector3D corner1, QVector3D corner2, QVector3D corner3);
     void drawRectangularPrism(GLfloat width, GLfloat height, GLfloat depth);
+    void addPointOrdering(QVector<GLuint> pointOrder, int numPoints);
     void drawRectangle(QVector3D llcorner, QVector3D lrcorner, QVector3D trcorner, QVector3D tlcorner);
+
+    int verticesAdded;
+
 
     QVector<GLfloat> m_data;
     int m_count;
