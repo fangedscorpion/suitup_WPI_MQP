ab01b5548e5f709cc79ab0aa2383fa1f56a04e58
Amanda Adkins <aaadkins@wpi.edu>
2015-12-11 20:59:04 -0500

    Update website to include enhanced drag functionality

--- public/dragonDrop.js
+++ public/dragonDrop.js
@@ -13,7 +13,7 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
     dragNum += 1;
-    dragonBox = document.getElementById("dragonDiv");
+    dragonBox = document.getElementById("marq2");
     currentHTML = dragonBox.innerHTML;
     dragonBox.innerHTML = currentHTML + '<img class="drag1" id="drag'+dragNum + '" src="http://thumbs.dreamstime.com/x/green-dragon-cartoon-illustration-11670789.jpg" draggable="true" ondragstart="drag(event)">'
 
