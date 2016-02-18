31bae1cb2fa0fdbdec7bafdf7d4748e85c7f5b2c
Matt Beardsley <mjbeardsley@wpi.edu>
2016-02-18 01:40:37 -0500

    add pretty comprehensive pose object tests. rewrite error function. take care of some memory leaks. add some trivial getter functions

--- software/GUI/WAG_bands/band/quatpose.cpp
+++ software/GUI/WAG_bands/band/quatpose.cpp
@@ -5,16 +5,20 @@ QuatPose::QuatPose(QVector3D translation, QVector3D rotatedTranslation, QVector3
     rt(rotatedTranslation),
     z(zAxis) {
     calibration = new QuatState(1,0,0,0);
-    points.reserve(3);
+    current = new QuatState(1,0,0,0);
+
+    const int npts = 3;
+    for (int i=0; i<npts; i++)
+        points.push_back(new QVector3D());
 }
 
 QuatState* QuatPose::qqinv(AbsState* q1, AbsState* q2) const {
     // multiplies q1 by the inverse of q2
     QQuaternion* qout = new QQuaternion();
-    QQuaternion q = *(static_cast<QuatState*>(q1))*static_cast<QuatState*>(q2)->inverted();
+    QQuaternion q = *((QuatState*)q1)*((QuatState*)q2)->inverted();
     qout->setScalar(q.scalar());
     qout->setVector(q.vector());
-    QuatState* qState = static_cast<QuatState*>(qout);
+    QuatState* qState = (QuatState*)qout;
     return qState;
 }
 
@@ -31,33 +35,29 @@ AbsState* QuatPose::adjust(AbsState* state) const {
 }
 
 IError* QuatPose::error(AbsState* goal) const {
-    QVector3D* errAxis = new QVector3D();
-    float* angle = new float();
-    QuatState* qCurrent = static_cast<QuatState*>(current);
+//    % returns swing and twist components of this quaternion about
+//    % axis pure quaternion axis
+//    % qerr = twist * swing
+//    axis = axis.v/norm(axis.v);
+//    p = dot(qerr.v, axis)*axis;
+//    twist = Quaternion([qerr.w; p]).unit();
+//    swing = inv(twist) * qerr;
+    QVector3D axis = rt.normalized();
+    QuatState* qerr = qqinv(goal,current);
 
-    // calculate error
-    QuatState* qErr = qqinv(goal,current);
-    // get error axis
-    qErr->getAxisAndAngle(errAxis, angle);
-    // rotate rt by current to get axis along joint
-    QVector3D axis = qCurrent->rotatedVector(rt.normalized());
-    // scale the joint axis by the length of its projection onto the error axis
-    axis *= QVector3D::dotProduct(*errAxis,axis);
+    QVector3D p = QVector3D::dotProduct(qerr->vector(),axis)*axis;
+    QQuaternion twist = QQuaternion(qerr->scalar(),p).normalized();
+    QQuaternion swing = twist.inverted() * (*qerr);
 
-    // err * twist * swing
-    // twist = rotation about joint axis
-    QQuaternion twist = QQuaternion(qErr->scalar(),axis).normalized();
-    // swing = rotation perpendicular to joint axis
-    QQuaternion swing = twist.inverted()*(*qErr);
-
-    return new QuatError(*qErr,swing,twist,qCurrent->rotatedVector(z),qCurrent->rotatedVector(rt));
+    return new QuatError(*qerr,swing,twist,((QuatState*)current)->rotatedVector(z),((QuatState*)current)->rotatedVector(rt));
 }
 
-void QuatPose::updatePoints(AbsState* parentState, QVector3D parentEndPoint) {
-    QVector3D transNoRot = static_cast<QuatState*>(parentState)->rotatedVector(t) + parentEndPoint;
-    QVector3D transTotal = transNoRot + static_cast<QuatState*>(current)->rotatedVector(rt);
+void QuatPose::updatePoints(AbsState* parentState, QVector3D* parentEndPoint) {
+    QVector3D transNoRot = ((QuatState*)parentState)->rotatedVector(t) + *parentEndPoint;
+    QVector3D transTotal = transNoRot + ((QuatState*)current)->rotatedVector(rt);
+
     points[0] = parentEndPoint;
-    points[1] = transNoRot;
-    points[2] = transTotal;
+    *(points[1]) = 1*transNoRot;
+    *(points[2]) = 1*transTotal;
 }
 
