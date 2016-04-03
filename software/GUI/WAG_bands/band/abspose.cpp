15127dc6176cada11eae6d08552be9a25c47428a
Samee-Swartz <Samee.swartz@gmail.com>
2016-04-03 00:06:58 -0400

    now it's done

--- software/GUI/WAG_bands/band/abspose.cpp
+++ software/GUI/WAG_bands/band/abspose.cpp
@@ -2,54 +2,7 @@
 
 AbsPose::AbsPose() : QObject()
 {
-node = 0;
 }
 
 AbsPose::~AbsPose(){
-    if (node != 0)
-        delete node;
 }
-
-//void AbsPose::update(AbsState *s){
-//     qDebug("Update");
-//}
-
-//QDataStream &operator<<(QDataStream &ds, const AbsPose &obj) {
-//    for(int i=0; i<obj.metaObject()->propertyCount(); ++i) {
-//        if(obj.metaObject()->property(i).isStored(&obj)) {
-//            ds << obj.metaObject()->property(i).read(&obj);
-
-//        }
-//    }
-//    return ds;
-//}
-//QDataStream &operator>>(QDataStream &ds, AbsPose &obj) {
-//    QVariant var;
-//    for(int i=0; i<obj.metaObject()->propertyCount(); ++i) {
-//        if(obj.metaObject()->property(i).isStored(&obj)) {
-//            ds >> var;
-//            obj.metaObject()->property(i).write(&obj, var);
-//        }
-//    }
-//    return ds;
-//}
-
-//QDataStream &operator<<(QDataStream &ds, const AbsState &obj) {
-//    for(int i=0; i<obj.metaObject()->propertyCount(); ++i) {
-//        if(obj.metaObject()->property(i).isStored(&obj)) {
-//            ds << obj.metaObject()->property(i).read(&obj);
-
-//        }
-//    }
-//    return ds;
-//}
-//QDataStream &operator>>(QDataStream &ds, AbsState &obj) {
-//    QVariant var;
-//    for(int i=0; i<obj.metaObject()->propertyCount(); ++i) {
-//        if(obj.metaObject()->property(i).isStored(&obj)) {
-//            ds >> var;
-//            obj.metaObject()->property(i).write(&obj, var);
-//        }
-//    }
-//    return ds;
-//}
